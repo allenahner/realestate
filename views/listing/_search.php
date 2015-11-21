64e75d0f73b6a4b93968846655cd41ef4dc10043
Allen Ahner <ahnera@missouri.edu>
2015-11-20 23:55:57 -0600

    Revised search partial render.

--- views/listing/_search.php
+++ views/listing/_search.php
@@ -11,7 +11,6 @@ use yii\widgets\ActiveForm;
 <div class="listing-search">
 
     <?php $form = ActiveForm::begin([
-        'action' => ['index'],
         'method' => 'get',
     ]); ?>
 
@@ -20,24 +19,6 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'listing_num') ?>
 
     <?= $form->field($model, 'description') ?>
-
-    <?= $form->field($model, 'img_path') ?>
-
-    <?= $form->field($model, 'sm_img_path') ?>
-
-    <?php // echo $form->field($model, 'price') ?>
-
-    <?php // echo $form->field($model, 'is_sold') ?>
-
-    <?php // echo $form->field($model, 'pg1_gallery_id') ?>
-
-    <?php // echo $form->field($model, 'create_user_id') ?>
-
-    <?php // echo $form->field($model, 'create_time') ?>
-
-    <?php // echo $form->field($model, 'update_user_id') ?>
-
-    <?php // echo $form->field($model, 'update_time') ?>
 
     <div class="form-group">
         <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
