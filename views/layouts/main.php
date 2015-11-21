0b1395b416f3a6c3e5ac0742fd17a308b1c65c2f
Allen Ahner <ahnera@missouri.edu>
2015-11-20 23:44:19 -0600

    Minor URL tweaks.

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -27,7 +27,7 @@ AppAsset::register($this);
 <div class="wrap">
     <?php
     NavBar::begin([
-        'brandLabel' => 'CJ Kiser Real Estate',
+        'brandLabel' => 'Real Estate',
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',
@@ -36,8 +36,7 @@ AppAsset::register($this);
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'Listings', 'url' => ['/listing']],
+            ['label' => 'Listings', 'url' => ['/listings']],
             ['label' => 'About', 'url' => ['/site/about']],
             ['label' => 'Contact', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest ?
